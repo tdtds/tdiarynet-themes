736099bf2a2e740bea49bdb5c1afbe6a2c80f014
TADA Tadashi <t@tdtds.jp>
2015-05-11 10:50:21 +0900

    update

--- public/genjson.rb
+++ public/genjson.rb
@@ -11,14 +11,10 @@ themes = {}
 Dir.glob('*/README') do |readme|
 	theme, = readme.split(/\//)
 	attrs = {}
-	begin
-		open(readme, &:read).split(/\n\n/m)[0].each_line do |l|
-			key, val = l.chomp.split(/:\s*/, 2)
-			attrs[key.downcase] = val
-		end
-		themes[theme] = attrs
-	rescue
-		$stderr.puts $!, readme
+	open(readme, &:read).split(/\n\n/m)[0].each_line do |l|
+		key, val = l.chomp.split(/:\s*/, 2)
+		attrs[key.downcase] = val
 	end
+	themes[theme] = attrs
 end
 print(JSON.pretty_generate({}.tap{|h| h['themes'] = themes}))
