7b11a0b85695d4a5c48bd97a54bca19dae07ffe7
TADA Tadashi <t@tdtds.jp>
2016-11-02 11:37:13 +0900

    redirect all files to GitHub pages

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ if (user && pass) {
 
 app.use(express.logger('dev'));
 app.use(express.compress());
-app.use(express.static(__dirname + '/public'));
+app.get(/.*/, function(req, res) {
+	res.redirect(301, "https://tdiary.github.io/tdiary-theme" + req.url);
+});
 
 app.listen(app.get('port'), function() {
   console.log('Server listening on port %s', app.get('port'));
